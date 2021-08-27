77619cdb22369a7947efd282eaa6a17fd3dfccfe
jagriti75 <51508071+jagriti75@users.noreply.github.com>
2021-08-27 12:05:57 +0530

    Add files via upload

---
+++ move.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        void move(int arr[],int n){
+            int k=0;
+            for(int i=0;i<n;i++){
+                if(arr[i]<0){
+                    swap(arr[k],arr[i]);
+                    k++;
+                }
+            }
+
+        }
+
+}; 
+
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+        int arr[1000];
+        int n;
+        cin>>n;
+        Solution obj;
+        obj.move(arr,n);
+        for(int i=0;i<n;i++){
+            cout<<arr[i]<<" ";
+        }
+  }
+  return 0;
+}
