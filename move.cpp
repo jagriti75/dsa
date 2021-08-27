72e75226c79d0b57c9ec39a3b49562cbd134564c
jagriti75 <51508071+jagriti75@users.noreply.github.com>
2021-08-27 12:45:51 +0530

    Update move.cpp

--- move.cpp
+++ move.cpp
@@ -23,6 +23,9 @@ int main(){
         int arr[1000];
         int n;
         cin>>n;
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
         Solution obj;
         obj.move(arr,n);
         for(int i=0;i<n;i++){
